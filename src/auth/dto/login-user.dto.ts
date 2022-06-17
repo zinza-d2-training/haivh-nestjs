bc7bac4da4d7ec16fd0eadbd8baeb060ff0082f0
HaiVu <vuhuyhai1102@gmail.com>
2022-06-17 11:29:43 +0700

    max length login-user.dto

--- src/auth/dto/login-user.dto.ts
+++ src/auth/dto/login-user.dto.ts
@@ -1,10 +1,13 @@
-import { IsEmail, IsNotEmpty } from 'class-validator';
+import { IsEmail, IsNotEmpty, MaxLength, MinLength } from 'class-validator';
 
 export class LoginUserDto {
   @IsNotEmpty({ message: 'Email can not be blank' })
   @IsEmail({ message: 'It must is email' })
+  @MaxLength(45)
   email: string;
 
   @IsNotEmpty({ message: 'Password can not be blank' })
+  @MinLength(8)
+  @MaxLength(45)
   password: string;
 }
