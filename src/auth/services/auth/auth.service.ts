1eed57cc66c35777a48e058db19794901af578cf
HaiVu <vuhuyhai1102@gmail.com>
2022-06-20 17:44:39 +0700

    hidden pass

--- src/auth/services/auth/auth.service.ts
+++ src/auth/services/auth/auth.service.ts
@@ -28,8 +28,13 @@ export class AuthService {
 
   async login(user: UserLogin) {
     const payload = { email: user.email, id: user.id };
+    const access_token = this.jwtService.sign(payload);
+    const { password, ...userInfo } = user;
     return {
-      access_token: this.jwtService.sign(payload),
+      user: {
+        ...userInfo,
+        access_token,
+      },
     };
   }
 
