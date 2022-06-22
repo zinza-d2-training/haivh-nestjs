9d23cf95a12986e4a0373c836313e3b37eda2104
HaiVu <vuhuyhai1102@gmail.com>
2022-06-22 14:37:12 +0700

    use env for be port

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,15 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as dotenv from 'dotenv';
+
+dotenv.config();
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
   app.setGlobalPrefix('api');
   app.enableCors();
-  await app.listen(5000);
+  await app.listen(process.env.BE_PORT);
 }
 bootstrap();
