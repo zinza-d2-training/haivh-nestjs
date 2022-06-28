5bdd0787da0cf7eaa94c88acb4de0f3d2a8b956e
HaiVu <vuhuyhai1102@gmail.com>
2022-06-28 14:34:50 +0700

    fix

--- src/auth/services/auth/auth.service.ts
+++ src/auth/services/auth/auth.service.ts
@@ -27,7 +27,7 @@ export class AuthService {
   }
 
   async login(user: UserRole) {
-    const payload = { email: user.email, id: user.id, role: user.role_id };
+    const payload = { email: user.email, id: user.id, role_id: user.role_id };
     const access_token = this.jwtService.sign(payload);
     const { password, ...userInfo } = user;
     return {
